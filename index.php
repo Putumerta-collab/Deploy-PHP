83b201179dec98c962e1a272cb44a09a65b610ee
Putumerta-collab <pmerta22@gmail.com>
2024-10-29 11:53:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $relay4 = $data['relay4'];
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>TESTIN GDEV PHP</title>
+    <title>TESTIN DEV PHP</title>
 
     <script type="text/javascript">
         //Relay1
@@ -134,7 +134,7 @@ $relay4 = $data['relay4'];
     </style>
 
 <div class="container text-center py-4">
-    <h2 class="judul">SMART TEMPLE BERBASIS IOT DENGAN ESP  <br> 32 UNTUK MENGONTROL LAMPU DAN PENGERAS SUARA PADA PURA DESA <br> STUDI KASUS PURA DESA DESA ADAT PENGULON</h2>
+    <h2 class="judul">PUJA KELELAHAN KEMARIN MEMBANGUN CANDI BERSAMA RORO JONGGRANG  <br> 32 UNTUK MENGONTROL LAMPU DAN PENGERAS SUARA PADA PURA DESA <br> STUDI KASUS PURA DESA DESA ADAT PENGULON</h2>
 </div>
 
 
